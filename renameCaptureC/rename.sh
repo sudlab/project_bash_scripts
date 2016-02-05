fd32ea75913b6a54f907aff42e3ea5fe46335c2e
Jaime A. Benayas <jaime.alvarez.benayas@gmail.com>
2016-02-05 22:29:36 +0000

    Add rename CaptureC

---
+++ renameCaptureC/rename.sh
@@ -0,0 +1,10 @@
+cd /home/mbp15ja/CaptureC_data/Sudlab
+rename -v 's/\IGF0003828\.bwa\.tsv\./P2.OPM2_/' *.gz
+rename -v 's/\IGF0003829\.bwa\.tsv\./P2.Jurkat_/' *.gz
+rename -v 's/\IGF0003830\.bwa\.tsv\./P2.U266_/' *.gz
+
+cd /home/mbp15ja/CaptureC_data/Perl
+rename -v 's/\IGF0003828\.bowtie_CC3_/P1.OPM2_/' *.wig
+rename -v 's/\IGF0003829\.bowtie_CC3_/P1.Jurkat_/' *.wig
+rename -v 's/\IGF0003830\.bowtie_CC3_/P1.U266_/' *.wig
+
